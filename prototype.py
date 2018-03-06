e3bf67e93a87de920dd6fbb46ff414c9ee1d0e18
Mark Harfouche <mark.harfouche@gmail.com>
2018-03-05 20:36:54 -0800

    Rewritten for clarity.

--- prototype.py
+++ prototype.py
@@ -1,15 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  5 17:52:56 2018
 
-import validity91
-from array import array
+@author: mark
+"""
+import matplotlib.pyplot as plt
 import numpy as np
+import validity91_2 as validity91
 
 import usb.core
 import usb.util
 import usb.control
 
 import usb
+
+from array import array
+
 import time
-import matplotlib.pyplot as plt
+
+np.set_printoptions()
+np.set_printoptions(threshold=np.inf,
+                    formatter={'int_kind': lambda i: f"0x{i:02x}"},
+                    linewidth=5 * 8 + 1)
+
 
 dev = usb.core.find(idVendor=0x138A, idProduct=0x0091)
 if dev is None:
@@ -17,154 +31,64 @@ if dev is None:
 
 dev.reset()
 
-
 cfg = dev.get_active_configuration()
 endpoints = cfg[(0, 0)]
 
 for endpoint in endpoints:
     endpoint.clear_halt()
 
-
-# I forget what this does
-assert(dev.ctrl_transfer(usb.util.CTRL_IN | usb.util.CTRL_TYPE_VENDOR |
-                         usb.util.CTRL_RECIPIENT_DEVICE, 20, 0, 0, 2) ==
-       array('B', [0, 0]))
-
-
-
 bulk_out = endpoints[0]  # Ednpoint 0x01, Direction OUT
 bulk_in = endpoints[1]    # Endpoint 0x81. Direction IN
-bulk_in2 = endpoints[2]    # Endpoint 0x82. Direction IN
 
 interrupt_in = endpoints[3] # Endpoint 0x83. Interrupt IN
 
-
-bulk_out.write(validity91.message1)
-response1 = bulk_in.read(64)
-assert(len(response1) == len(validity91.response1))
-
-bulk_out.write(validity91.message2)
-response2 = bulk_in.read(68)
-# not the same
-#assert(response2 == validity91.response2)
-
-bulk_out.write(validity91.message3)
-response3 = bulk_in.read(64)
-assert(response3 == validity91.response3)
-
-bulk_out.write(validity91.message4)
-response4 = bulk_in.read(64)
-assert(response4 == validity91.response4)
-
-bulk_out.write(validity91.message5)
-response5 = bulk_in.read(64)
-assert(response5 == validity91.response5)
-
-bulk_out.write(validity91.message6)
-response6 = bulk_in.read(64)
-assert(len(response6) == len(validity91.response6))
-
-bulk_out.write(validity91.message7)
-response7 = bulk_in.read(64)
-assert(response7 == validity91.response7)
-
-bulk_out.write(validity91.message8)
-response8 = bulk_in.read(4096)
-assert(len(response8) == 1962)  # this seems to be consistent
-# assert(response8 == validity91.response8)
-
+# %%
+"""
+# is this needed?
+assert(dev.ctrl_transfer(usb.util.CTRL_IN | usb.util.CTRL_TYPE_VENDOR |
+                     usb.util.CTRL_RECIPIENT_DEVICE, 20, 0, 0, 2) ==
+       validity91.success)
+"""
+time.sleep(0.05)
+for i, message in enumerate(validity91.init_messages):
+    try:
+        response = message.send(bulk_out, bulk_in)
+        message.check(response)
+    except AssertionError:
+        print(f'Message {i}')
+        raise
+i = None
 
 # %%
+
+response = validity91.acquisition_start_message.send(bulk_out, bulk_in)
+validity91.acquisition_start_message.check(response)
+
 int_response1 = interrupt_in.read(8)
-assert(int_response1 == array('B', [0, 0, 0, 0, 0]))
-# Should return 0 (It is ready to take inputs??)
-# %%
-# By setting the timeout to 0, it will notify you that a finger has been put down
-print("Waiting for you to put your finger")
-r = interrupt_in.read(8,timeout=0)
-print(r)
-print("You may now remove your finger!")
-# %%
-def capture_finger(bulk_out, bulk_in):
-    for i in range(10000):
-        bulk_out.write(validity91.message_is_image_ready)
-        res = bulk_in.read(64)
-        # res == 0s seems to indicate that data is ready
-        # res == 1, 0, 0, 0, 0 seems to indicate that data isn't ready
-
-        if res[0] == 0:
-            print("Data ready")
-
-            bulk_out.write(validity91.message_read_image_part)
-
-            # You can read the data in small chunks
-            img1 = bulk_in.read(4096)
-
-            # 6 first bytes are descriptive
-            # They contain the length of the remaining array
-            frame_size = img1[3]*256 + img1[2]
-            if frame_size > len(img1) - 6:
-                img1 = img1 + bulk_in.read(4096)
-
-            # or you can read them all in 1 shot if you know exactly how
-            # many bytes are coming in
-            bulk_out.write(validity91.message_read_image_part)
-            img2 = bulk_in.read(4806)
-
-            bulk_out.write(validity91.message_read_image_part)
-            img3 = bulk_in.read(4806)
-
-            # put the 3 arrays together
-            img = np.array(img1[6:] + img2[6:] + img3[6:])
-            # remove the blanking lines (maybe these mean something important)
-            img = img.reshape(-1, 120)[:, 8:]
-            return img
-        elif res[0] == 1:
-            # print("Data not ready")
-            pass
-        else:
-            print(res)
-
-    raise RuntimeError("Could not capture a fingerprint")
+assert(int_response1 == validity91.interrupt_ready_response)
 
 
-img = capture_finger(bulk_out, bulk_in)
+print('Sensor ready, put your finger on')
+int_response2 = interrupt_in.read(8, timeout=0)
+print(f'Interrupt response 2: length {len(int_response2)}')
+print(np.array(int_response2))
 
-# %%
-# plot it if you wish
-fig = plt.figure('First capture')
+img = validity91.read_image(bulk_out, bulk_in)
+
+
+for i, message in enumerate(validity91.stop_acquisition):
+    try:
+        response = message.send(bulk_out, bulk_in)
+        message.check(response)
+    except AssertionError:
+        print(f'Message {i}')
+        raise
+i = None
+
+fig = plt.figure('Your fingerprint')
 ax = fig.gca()
 ax.imshow(img, vmin=0, vmax=255)
-#plt.colorbar()
-
-# %%
-
-bulk_out.write(validity91.repeated_message1)
-repeated_response1 = bulk_in.read(64)
-assert(repeated_response1 == validity91.repeated_response1)
-
-
-bulk_out.write(validity91.repeated_message2)
-repeated_response2 = bulk_in.read(64)
-assert(repeated_response2 == validity91.repeated_response2)
-# %%
-# Is this necessary
-# The wireshark capture showed that it sent out a bulk_in2 read.
-# but this only throws me an error
-# res = bulk_in2.read(1024)
-# print(res)
-
-# %%
-
-
-input("Remove your finger, then press enter to take a second print")
-response8 = None
-bulk_out.write(validity91.message8)
-response8 = bulk_in.read(4096)
-# assert(response8 == validity91.response8)
-
-
-img2 = capture_finger(bulk_out, bulk_in)
-fig = plt.figure('Second capture')
-ax = fig.gca()
-ax.imshow(img2, vmin=0, vmax=255)
+ax.set_title('Your fingerprint')
+ax.set_xlabel('x (pixel)')
+ax.set_ylabel('y (pixel)')
+# fig.dpi = 200  # set to 200 for high dpi screens
